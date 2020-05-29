fb7367e930ba3c13af944c2e5d45e97651bede59
Ryan Paroz <ryan.paroz@gmail.com>
2020-05-29 19:28:53 +1000

    Modify files for new project

--- setup.py
+++ setup.py
@@ -1,26 +1,21 @@
 from setuptools import setup
 
 GEN_version = '0.0.6'
-READ_name = 'decpac'
+READ_name = 'pacman-deck'
 
 setup(
     name=READ_name,
     version=GEN_version,
-    author='rendaw',
-    url='https://gitlab.com/rendaw/decpac',
-    download_url='https://gitlab.com/rendaw/decpac/-/archive/v{v}/decpac-v{v}.tar.gz'.format(
-        v=GEN_version),
+    author='TPHRyan',
+    url='https://github.com/TPHRyan/pacman-deck',
     license='MIT',
-    description='Arch Linux declarative package management',
-    long_description=open('readme.md', 'r').read(),
+    description='A transparent approach to declarative package management in Arch.',
+    long_description=open('README.md', 'r').read(),
     classifiers=[],
-    py_modules=['decpac'],
-    install_requires=[
-        'luxem==0.0.2',
-    ],
+    py_modules=['pacman-deck'],
     entry_points={
         'console_scripts': [
-            'decpac=decpac:main',
+            'deck=pacman-deck:main',
         ],
     },
 )
